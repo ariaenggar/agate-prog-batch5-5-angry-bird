96cbded34c7f036228470c30c6f7a91e3876757a
ariaenggar <aria.enggar@gmail.com>
2021-11-13 18:55:39 +0700

    init

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        string tag = col.gameObject.tag;
+        if (tag == "Bird" || tag == "Enemy" || tag == "Obstacle")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
